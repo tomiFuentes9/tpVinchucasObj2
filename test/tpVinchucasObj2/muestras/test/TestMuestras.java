9a56451d028c1a3c26e7dedeaf7c55b5c2543f11
Tomas Fuentes <tomasrfuentes@gmail.com>
2023-05-27 00:44:12 -0300

    Prueba

---
+++ test/tpVinchucasObj2/muestras/test/TestMuestras.java
@@ -0,0 +1,19 @@
+package tpVinchucasObj2.muestras.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestMuestras {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
