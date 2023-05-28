4fdb652684f7be2e4e6b45c994dcb3ea5795ae23
walterggomez <walterggomez@gmail.com>
2023-05-27 21:57:30 -0300

    Agregado de clase Participante

--- src/tpVinchucasObj2/participantes/Participante.java
+++ src/tpVinchucasObj2/participantes/Participante.java
@@ -1,5 +1,31 @@
 package tpVinchucasObj2.participantes;
+import tpVinchucasObj2.muestras.Muestra;
+import tpVinchucasObj2.muestras.opinion.Opinion;
 
 public class Participante {
-	private String nombre ;
+	private String nombre;
+	private EstadoUsuario estadoParticipante;
+	
+	public Participante(String nombre, EstadoUsuario estadoParticipante) {
+		super();
+		this.nombre = nombre;
+		this.estadoParticipante = estadoParticipante;
+	}
+	public String getNombre() {
+		return nombre;
+	}	
+	public Muestra enviarMuestra() {
+		return null;
+	}
+	public Opinion opinarMuestra(Muestra nuestra) {
+		return null;
+	}
+	public void evaluarEstado() {
+		
+	}
+	
+	public String estadoActual() {
+		return estadoParticipante.toString();
+	}
+
 }
