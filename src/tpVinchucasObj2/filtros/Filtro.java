9bd195384a1202388644d2df0173ba756c1bf704
Tomas Fuentes <tomasrfuentes@gmail.com>
2023-06-11 16:31:26 -0300

    Filtro y filtro por fecha creados

---
+++ src/tpVinchucasObj2/filtros/Filtro.java
@@ -0,0 +1,10 @@
+package tpVinchucasObj2.filtros;
+
+import java.util.*;
+
+import tpVinchucasObj2.muestra.Muestra;
+
+public abstract class Filtro {
+	
+	public abstract List<Muestra> filtrarMuestras(List<Muestra> muestras);
+}
